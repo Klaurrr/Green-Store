e1660594ac2e3e16f345422f89e87f2c37b99426
Klaurrr <usachev-85@yandex.ru>
2023-04-28 22:35:57 +0300

    фикс багов

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,16 +1,15 @@
-import Layout from "@/components/templates/Layout";
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-    return (
-        <Html lang="en">
-            <Head>
-                <title>Green Shop</title>
-            </Head>
-            <body>
-                <Main />
-                <NextScript />
-            </body>
-        </Html>
-    );
+      return (
+            <Html lang="en">
+                  <Head>
+                        <title>Green Shop</title>
+                  </Head>
+                  <body>
+                        <Main />
+                        <NextScript />
+                  </body>
+            </Html>
+      );
 }
