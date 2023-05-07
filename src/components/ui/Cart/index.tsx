93f0ae2093745d9a8d387191b9cb2ef5154a03de
Klaurrr <UsachevDev@yandex.ru>
2023-05-07 15:53:21 +0300

    Поменял импорт (тестовый коммит)

--- src/components/ui/Cart/index.tsx
+++ src/components/ui/Cart/index.tsx
@@ -2,19 +2,19 @@ import { usePlantsStore } from "@/store";
 
 import { IPlantsState } from "@/types/IPlantsState";
 
-import CartSvg from "../../../../public/assets/svg/CartSvg.svg";
+import CartSvg from "@/../../public/assets/svg/CartSvg.svg";
 
 import styles from "@/styles/components/ui/Cart.module.scss";
 
 const Cart = () => {
-      const plants = usePlantsStore((state: IPlantsState) => state?.cart);
+	const plants = usePlantsStore((state: IPlantsState) => state?.cart);
 
-      return (
-            <div className={styles.container}>
-                  <CartSvg />
-                  <div className={styles.counter}>{plants.length}</div>
-            </div>
-      );
+	return (
+		<div className={styles.container}>
+			<CartSvg />
+			<div className={styles.counter}>{plants.length}</div>
+		</div>
+	);
 };
 
 export default Cart;
