6ef0163a7af18a446ad2ffdd667ab8b863e3c716
Klaurrr <usachev-85@yandex.ru>
2023-07-31 21:31:55 +0300

    Попытка пофиксить баг с аутентификацией...

--- src/configs/auth.ts
+++ src/configs/auth.ts
@@ -35,7 +35,7 @@ export const authConfig: AuthOptions = {
                   },
             }),
       ],
-      // pages: {
-      //       signIn: "/Home",
-      // },
+      pages: {
+            signIn: "/Home",
+      },
 };
